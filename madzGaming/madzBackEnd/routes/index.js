77e7f8ba17c7b43840015d88cbed1b37e8cc3464
MadianH <madianh33@gmail.com>
2019-01-11 14:28:28 +0100

    ajustement des autorisations du sign up

--- madzGaming/madzBackEnd/routes/index.js
+++ madzGaming/madzBackEnd/routes/index.js
@@ -11,30 +11,41 @@ mongoose.connect('mongodb://madz:madz73@ds149414.mlab.com:49414/madzgaming', opt
 var userSchema = mongoose.Schema({ pseudo: String, email: String, password: String,});
 var userModel = mongoose.model('user', userSchema);
 
+
+// Vérifie que le pseudo comporte entre 3 et 20 caractéres
+const pseudoIsValid = pseudo => {
+  let isValid = false
+  if(pseudo.length >= 3 && pseudo.length <= 20){
+    isValid = true
+  }
+  return isValid;
+};
+
+// Vérifie que le password comporte entre 4 et 15 caractéres
+const passwordIsValid = password => {
+  let isValid = false
+  if(password.length >= 4 && password.length <= 15){
+    isValid = true
+  }
+  return isValid;
+};
+
+
 router.post('/signup', function(req, res, next) {
 
-  let emailInspect = req.body.email;
-  let atExist = false;
-  let atValid = false;
   let isExist = false;
+  let readyToDb = false
 
-  if(emailInspect.indexOf("@") < 0){
-    res.json({ atExist });
-  } else if (emailInspect.indexOf("@") >= 0){
-
-    if(emailInspect.indexOf("@") >= 3){
-        if(emailInspect.indexOf(".") >= 0){
-          atValid = true;
-          console.log(atValid);
-        }else{
-          res.json({ atValid });
-        }
-    }else{
-      res.json({ atValid });
-     }
+  if(!pseudoIsValid(req.body.pseudo)){
+    res.json({ pseudoValid: false});
+  }
+  else if(!passwordIsValid(req.body.password)){
+    res.json({ passwordValid: false});
+  }else{
+    readyToDb = true
   }
 
-  if(atValid){
+  if (readyToDb) {
     userModel.find(
       { email: req.body.email } ,
       function (err, users) {
@@ -44,8 +55,19 @@ router.post('/signup', function(req, res, next) {
             email: req.body.email,
             password: req.body.password,
           });
-          newUser.save(function(error, user) {
-            res.json({ isExist, user });
+          newUser.save(
+            function(error, user) {
+              if (err){
+                res.json({
+                  signup : false,
+                  result : err
+                })
+              } else {
+                res.json({
+                  signup : true,
+                  result : user,
+                })
+              }
           });
         }
         else if (users.length > 0){
@@ -54,8 +76,6 @@ router.post('/signup', function(req, res, next) {
         }
       }
     )
-  }else{
-    res.json({ atValid });
   }
 });
 
