eeb50703fde11dcac8f7d24a64e7d9637139b056
MadianH <madianh33@gmail.com>
2019-01-18 18:32:21 +0100

    ajout de nodemailer et de function permettant d'envoyer des mails

--- madzGaming/madzBackEnd/routes/index.js
+++ madzGaming/madzBackEnd/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+const nodemailer = require("nodemailer");
 var router = express.Router();
 
 
@@ -8,8 +9,59 @@ mongoose.connect('mongodb://madz:madz73@ds149414.mlab.com:49414/madzgaming', opt
   console.log(err);
 });
 
+
+
 var userSchema = mongoose.Schema({ pseudo: String, email: String, password: String,});
 var userModel = mongoose.model('user', userSchema);
+
+
+// Envoi email
+const sendEmail = email => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "in-v3.mailjet.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "379270f8f4bc72df51cc38464d0e97ed", // generated ethereal user
+      pass: "9131b5778727875637ef0627ec57af00" // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  transporter.verify(function(error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Madz Company" <web.madz.company@gmail.com>', // sender address
+    to: email, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "test 2", // plain text body
+    html: "<b>Hello world?</b>" // html body
+   };
+
+  // send mail with defined transport object
+   transporter.sendMail(mailOptions, (error, info) => {
+     if(error){
+       return console.log(error);
+     }else {
+       console.log(info);
+     }
+   })
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+};
+
 
 
 // Vérifie que le pseudo comporte entre 3 et 20 caractéres
